d42a3aa771fa99f212b6259e1ee76536d16c949e
山木祥平 <yamaki817122@gmail.com>
2017-11-03 03:05:10 +0900

    編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,13 +14,32 @@ class PostsController < ApplicationController
 		if @post.save
 			redirect_to posts_path
 		else
-			render new
+			render 'new'
 		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
 	end 
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to posts_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
 
 
 private 
